2fd230b484a6aeda89cfff2e56c2a11c3c2a8c6a
Neeraj R <neerajrk007@gmail.com>
2021-06-14 21:31:48 +0530

    Scaffold removed from CategoryScreen

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -7,11 +7,7 @@ class CategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ramlas Kitchen nrj'),
-      ),
-      body:GridView(
+    return GridView(
         padding:EdgeInsets.all(20) ,
       children: DUMMY_CATEGORIES.map((catData)=>CategoryItem(catData.id,catData.title,catData.color)).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
@@ -21,8 +17,6 @@ class CategoryScreen extends StatelessWidget {
         crossAxisSpacing: 20,
       ),
       
-    )
-     
     );
   }
 }
